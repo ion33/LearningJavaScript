c7fb9ab5513d7065f251ef14edd41144809eeb5f
Bnarcis7 <basarlicinarcis@yahoo.com>
2017-03-24 00:13:03 +0200

    23/03/2017

--- HeadFirstJavascript/pagina10.js
+++ HeadFirstJavascript/pagina10.js
@@ -1,10 +1,17 @@
-var age=25;
-var name: "Owen";
-if(age>14)
+function caffe() {
+
+var persoana={age:11, name:"Joe", surname:"Michaels"};
+var persoana2=persoana;
+persoana2.name="Narcis";
+persoana.surname=persoana2.name;
+if(persoana2.age>14)
 {
     alert("Sorry this is a page for kids!");
 }
 else
 {
-    alert("Welcome " + name + ");
+    alert("Welcome " + persoana.surname +" "+ persoana.name +"!");
+    alert("Welcome " + persoana2.surname +" "+ persoana2.name +"!");
 }
+}
+caffe();
