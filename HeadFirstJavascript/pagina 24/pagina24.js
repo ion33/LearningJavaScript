f8a33c801cb9a28419242328b283fc378134d2ff
Bnarcis7 <basarlicinarcis@yahoo.com>
2017-03-28 16:29:21 +0300

    429

--- HeadFirstJavascript/pagina 24/pagina24.js
+++ HeadFirstJavascript/pagina 24/pagina24.js
@@ -5,7 +5,7 @@ function HappyBirthday(){
         document.write("Happy birthday to you. <br>");
         i=i+1;
     }
-    document.write("Happy birthday dear " +name+"<br>");
+    document.write("Happy birthday dear " +name+",<br>");
     document.write("Happy birthday to you. <br>");
 }
 HappyBirthday();
